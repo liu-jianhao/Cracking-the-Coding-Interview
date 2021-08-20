4740c682275feaf1d34f740aa4b5266e6d4d2233
liujianhao <liujianhao@bytedance.com>
2021-08-20 20:52:48 +0800

    02.04.分割链表
    Change-Id: I2e602ecfe11f619366ccd40fbbd78480a158f3b1

---
+++ 02.04.分割链表/02.04.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* small = new ListNode(0);
+        ListNode* large = new ListNode(0);
+        ListNode* smallHead = small;
+        ListNode* largeHead = large;
+
+        while (head != nullptr) {
+            if (head->val < x) {
+                small->next = head;
+                small = small->next;
+            } else {
+                large->next = head;
+                large = large->next;
+            }
+            head = head->next;
+        }
+        large->next = nullptr;
+        small->next = largeHead->next;
+        return smallHead->next;
+    }
+};
---
+++ 02.04.分割链表/02.04.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func partition(head *ListNode, x int) *ListNode {
+    small, large := &ListNode{}, &ListNode{}
+    smallHead, largeHead := small, large
+    for head != nil {
+        if head.Val < x {
+            small.Next = head
+            small = small.Next
+        } else {
+            large.Next = head
+            large = large.Next
+        }
+        head = head.Next
+    }
+    large.Next = nil
+    small.Next = largeHead.Next
+    return smallHead.Next
+}
