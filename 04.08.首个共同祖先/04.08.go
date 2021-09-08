b35972048ff2bc6e43eb61719251084c78d0fec2
liujianhao <liujianhao@bytedance.com>
2021-09-08 23:51:12 +0800

    04.08
    Change-Id: Iea4faf1e351a4becc4e4f9587fd5d59e19dda353

---
+++ 04.08.首个共同祖先/04.08.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func lowestCommonAncestor(root *TreeNode, p *TreeNode, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root == p || root == q {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left != nil && right != nil {
+		return root
+	}
+
+	if left == nil {
+		return right
+	}
+	return left
+}
